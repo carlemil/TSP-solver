890ba19f55a331436d2d6568a8b0376746b2d272
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-06-26 15:08:30 +0200

    cluster moving initiated.

--- src/TSPSolver.java
+++ src/TSPSolver.java
@@ -19,38 +19,72 @@ public class TSPSolver {
 
         double globalBest = Integer.MAX_VALUE;
         for (int n = 0; n < 1; n++) {
-            rndSeed = System.currentTimeMillis();
-            rndSeed = 1;
+            rndSeed = System.currentTimeMillis() + n;
+            rndSeed = 7;
             Tools.getRandomizedStartPath(path, rndSeed);
 
-            double last = Integer.MAX_VALUE - 1;
-            double best = Integer.MAX_VALUE;
-            while (last < best) {
-                best = last;
-                last = tryRemoveAndInsert(arcs, path);
-                double x = removeXarcs(path, arcs);
-                last = last > x ? x : last;
-            }
-            if (globalBest > best) {
-                globalBest = best;
+            double bestN3 = n3optimizations(arcs, path);
+
+            if (globalBest > bestN3) {
+                globalBest = bestN3;
                 Tools.checkPath(arcs, path);
                 for (int i = 0; i < path.length; i++) {
                     bestPath[i] = path[i];
                 }
 
-                Tools.savePathToFile(bestPath, rndSeed,
-                        "result_" + size + "_" + Tools.getPathLength(arcs, bestPath) + ".csv");
+                Tools.savePathToFile(bestPath, rndSeed, "result_" + size + "_" + Tools.getPathLength(arcs, bestPath) + ".csv");
             }
         }
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                Tools.createAndShowGUI(Tools.getPolygonForPlotting(data, bestPath, WINDOW_SIZE), WINDOW_SIZE,
-                        "TSP path, nodes: " + size + ", length: " + Tools.getPathLength(arcs, bestPath));
+                Tools.createAndShowGUI(Tools.getPolygonForPlotting(data, bestPath, WINDOW_SIZE), WINDOW_SIZE, "TSP path, nodes: "
+                        + size + ", length: " + Tools.getPathLength(arcs, bestPath));
             }
         });
     }
 
-    private static double tryRemoveAndInsert(double[][] arcs, int[] path) {
+    private static double n3optimizations(final double[][] arcs, final int[] path) {
+        double bestN2 = n2optimizations(arcs, path);
+        // moveClusters(arcs, path);
+        return bestN2;
+    }
+
+    private static void moveClusters(final double[][] arcs, final int[] path) {
+        int pl = path.length;
+
+        // cluster length
+        int cl = 2;
+        int[] c = new int[cl];
+
+        // cluster starts at node n
+        for (int n = 0; n < pl - cl; n++) {
+
+            // copy cluster to its own array
+            for (int cc = 0; cc < cl; cc++) {
+                c[cc] = path[n + cc];
+            }
+
+            // loop over locations to insert path
+            for (int i = 0; i < pl; i++) {
+                // loop over cluster and try to insert using different start/end nodes
+
+            }
+        }
+    }
+
+    private static double n2optimizations(final double[][] arcs, final int[] path) {
+        double last = Integer.MAX_VALUE - 1;
+        double best = Integer.MAX_VALUE;
+        while (last < best) {
+            best = last;
+            last = tryRemoveAndInsertOneNode(arcs, path);
+            double x = removeXarcs(path, arcs);
+            last = last > x ? x : last;
+        }
+        return best;
+    }
+
+    private static double tryRemoveAndInsertOneNode(double[][] arcs, int[] path) {
         int l = path.length;
 
         for (int n = 0; n < l; n++) {
@@ -63,23 +97,28 @@ public class TSPSolver {
                 double distOrg = arcs[pna][pnb] + arcs[pnb][pnc] + arcs[pib][pic];
                 double distNew = arcs[pnb][pib] + arcs[pnb][pic] + arcs[pna][pnc];
                 if (distOrg > distNew) {
-                    if (n < i) {
-                        int v = path[n];
-                        for (int j = n; j < i; j++) {
-                            path[j] = path[j + 1];
-                        }
-                        path[i] = v;
-                    } else if (n > i) {
-                        int v = path[n];
-                        for (int j = n; j > i + 1; j--) {
-                            path[j] = path[j - 1];
-                        }
-                        path[i + 1] = v;
-                    }
+                    moveNodeFromAToB(path, n, i);
                 }
             }
         }
         return Tools.getPathLength(arcs, path);
+    }
+
+    private static void moveNodeFromAToB(int[] path, int a, int b) {
+        if (a < b) {
+            int node = path[a];
+            for (int j = a; j < b; j++) {
+                path[j] = path[j + 1];
+            }
+            path[b] = node;
+        } else if (a > b) {
+            int node = path[a];
+            for (int j = a; j > b + 1; j--) {
+                path[j] = path[j - 1];
+            }
+            path[b + 1] = node;
+
+        }
     }
 
     private static double removeXarcs(int[] path, double[][] arcs) {
